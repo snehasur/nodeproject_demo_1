1e47f79ca7cce11f6b687570b4b654783263dab3
Sneha Sur <snehasurece@gmail.com>
2024-04-01 19:30:40 +0530

    commit

---
+++ chat-app-master/server/db/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+//const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.zw6hky5.mongodb.net/?retryWrites=true&w=majority`;
+const url =
+  "mongodb+srv://snehasur:JzPrdV2YOLxPs1Ui@snehacluster.mfdpq5b.mongodb.net/chatapp?retryWrites=true&w=majority";
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to DB"))
+  .catch((e) => console.log("Error", e));
